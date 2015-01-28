14760042930b401d409f7173e958d6b468f9dd5b
Julio Olivera <julio.olvr@gmail.com>
2015-01-27 21:35:54 -0300

    Base commit: Gulp + Sass

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    sass = require('gulp-sass');
+
+gulp.task('move', function() {
+  gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('./src/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'));
+});
+
+gulp.task('server', ['move', 'sass'], function() {
+  gulp.src('build')
+    .pipe(webserver());
+});
+
+gulp.watch('./src/**/*.html', ['move']);
+gulp.watch('./src/styles/**/*.scss', ['sass']);
+
+gulp.task('default', ['server'], function() {});
