5863cb8f4f909c62ffcc068e125fcb732090534b
Julio Olivera <julio.olvr@gmail.com>
2015-02-07 17:07:02 -0300

    Calendar swipe

--- src/js/components/calendar.jsx
+++ src/js/components/calendar.jsx
@@ -1,16 +1,39 @@
 'use strict';
 
 import React from 'react';
+import Swipe from 'react-swipe';
 import AttendeeList from './attendeeList.jsx';
 
 export default React.createClass({
   render() {
     return (
       <div className="calendar">
-        <div className="day">
-          Friday 9/13
-        </div>
-        <AttendeeList attendees={this.props.attendees}/>
+        <Swipe continuous={false}>
+          <div className="day">
+            <div className="day--date">
+              Friday 9/13
+            </div>
+            <AttendeeList attendees={this.props.attendees}/>
+          </div>
+          <div className="day">
+            <div className="day--date">
+              Saturday 9/14
+            </div>
+            <AttendeeList attendees={this.props.attendees}/>
+          </div>
+          <div className="day">
+            <div className="day--date">
+              Sunday 9/15
+            </div>
+            <AttendeeList attendees={this.props.attendees}/>
+          </div>
+          <div className="day">
+            <div className="day--date">
+              Monday 9/16
+            </div>
+            <AttendeeList attendees={this.props.attendees}/>
+          </div>
+        </Swipe>
       </div>
     );
   }
